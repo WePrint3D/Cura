e9034f37daceda40d2908c6ae17eb0f72c69d89f
Antoine <amartineau99@outlook.com>
2023-11-08 21:16:53 -0500

    Changed the vaporisation parameter to have layer selection and wait

--- plugins/PostProcessingPlugin/scripts/ExtrusionControl.py
+++ plugins/PostProcessingPlugin/scripts/ExtrusionControl.py
@@ -10,11 +10,6 @@ import re #To perform the search and replace
 from typing import List
 from ..Script import Script
 
-# TODO: Control of the Water Vaporizer 
-# TODO: Define the Extruder control Parameter
-#           - Voltage control
-#           - Vaporizer time
-
 class ExtrusionControl(Script):
 
     """Performs a search-and-replace on all g-code.
@@ -59,6 +54,26 @@ class ExtrusionControl(Script):
                     "type": "bool",
                     "default_value": false
                 },
+                "Vaporisation_each":
+                {
+                    "label": "Vaporisation each k layer",
+                    "description": "Choose the number of layer between each vaporisation.",
+                    "type": "enum",
+                    "options": {
+                        "1": "1",
+                        "2": "2",
+                        "3": "3",
+                        "4": "4",
+                        "5": "5",
+                        "6": "6",
+                        "7": "7",
+                        "8": "8",
+                        "9": "9",
+                        "10": "10"
+                    },
+                    "default_value": "1",
+                    "enabled": "Vaporisation_enable"
+                },
                 "M_vaporiation_start":
                 {
                     "label": "M code to start vaporisation",
@@ -80,6 +95,14 @@ class ExtrusionControl(Script):
                     "label": "Vapor time",
                     "description": "Time of activation of the water vaporisation between layers in miliseconds.",
                     "type": "str",
+                    "default_value": "1000",
+                    "enabled": "Vaporisation_enable"
+                },
+                "Vaporisation_wait":
+                {
+                    "label": "Vapor wait to print time",
+                    "description": "Time to wait before the restart of the print in miliseconds.",
+                    "type": "str",
                     "default_value": "2000",
                     "enabled": "Vaporisation_enable"
                 }
@@ -95,14 +118,14 @@ class ExtrusionControl(Script):
         M_vaporiation_start = self.getSettingValueByKey("M_vaporiation_start")
         M_vaporiation_stop = self.getSettingValueByKey("M_vaporiation_stop")
         Vaporisation_time = self.getSettingValueByKey("Vaporisation_time")
-        
+        Vaporisation_wait = self.getSettingValueByKey("Vaporisation_wait")
+        Vaporisation_each = int(self.getSettingValueByKey("Vaporisation_each"))
+
         init_state = 0
         extruder_state = 0
 
         for layer_number,layer in enumerate(data):
             lines = layer.split("\n")
-            # if Vaporisation_enable:
-            #     lines[0] = M_vaporiation + "\n" + "G4 P" + Vaporisation_time + "\n" + M_vaporiation + "\n" +  lines[0]
             null_lines = []
             for line_number, line in enumerate(lines):
                 
@@ -119,13 +142,12 @@ class ExtrusionControl(Script):
                     lines[line_number] = ''
                     null_lines.append(line_number)
                 elif 'G1' in line and ('X' in line or 'Y' in line) and 'E' in line and extruder_state == 0: # Add the start extrusion
-                    lines[line_number] = M_activate + "\n" + line.split('E')[0]# + ";===== Start of extrusion ======"
+                    lines[line_number] = M_activate + "\n" + line.split('E')[0] + ";=== Start of extrusion ==="
                     extruder_state = 1
                 elif 'G1' in line and 'E' in line:
-                    lines[line_number] = line.split('E')[0] # + ";======= Continue extrusion ====== "
-                #     # lines[line_number] = ''
+                    lines[line_number] = line.split('E')[0] + ";=== Extrusion ==="
                 elif 'G1' in line and not 'E' in line and extruder_state == 1:
-                    lines[line_number] = M_deactivate + "\n" + "G4 P" + Extrusion_stop_time + "\n" + line.split('E')[0]# + ";======Stop of extrusion======"
+                    lines[line_number] = M_deactivate + "\n" + "G4 P" + Extrusion_stop_time + "\n" + line.split('E')[0] + ";=== Stop of extrusion ==="
                     extruder_state = 0   
                 elif 'M104' in line or 'M105' in line or 'M106' in line or 'M107' in line or 'M109' in line or 'M82' in line or 'M83' in line:
                     lines[line_number] = ''
@@ -140,10 +162,17 @@ class ExtrusionControl(Script):
                     else: lines[line_number] = line
 
                 if Vaporisation_enable and 'LAYER:' in line:
-                    lines[line_number] = M_vaporiation_start + "\n" + "G4 P" + Vaporisation_time + "\n" + M_vaporiation_stop + "\n" + line.split('\n')[0]
+                    if (int(line.split('LAYER:')[1]) % Vaporisation_each) == 0:
+                        lines[line_number] = M_vaporiation_start + "\n" + "G4 P" + Vaporisation_time + "\n" + M_vaporiation_stop + "\n"
+                        if int(Vaporisation_wait) > 0:
+                            lines[line_number] += "G4 P" + Vaporisation_wait + "\n" +  line.split('\n')[0]
+                        else:
+                            lines[line_number] += line.split('\n')[0]
+                    else:
+                        lines[line_number] = line.split('\n')[0]
             
-            # for i in sorted(null_lines, reverse=True): 
-            #     lines.pop(i)
+            for i in sorted(null_lines, reverse=True): 
+                lines.pop(i)
             new_layer = "\n".join(lines)
             data[layer_number] = new_layer
         return data
