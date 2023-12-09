8c8e971e46df48654ab41f28bdc44e3dda9d6872
Antoine <amartineau99@outlook.com>
2023-12-09 14:56:50 -0500

    Update Tool order

--- plugins/PostProcessingPlugin/scripts/ToolOrder.py
+++ plugins/PostProcessingPlugin/scripts/ToolOrder.py
@@ -69,14 +69,21 @@ class ToolOrder(Script):
                 #Create a str from layer after the first tool line number to the last four "\n" lines before ";TIME_ELAPSED:"
                 n_layer = '\n'.join(lines[1:])
                 new_layer = lines[0] + "\n" + first_tool + "\n" + n_layer.split(first_tool)[1]
-                prev_last_four_first = '\n'.join(lines[-5:])
+                # Find the last line containing "Z"
+                for line_number, line in enumerate(lines):
+                    if "Z" in line:
+                        last_z_line = line_number
+                prev_last_four_first = '\n'.join(lines[last_z_line:])
                 new_layer = new_layer.split(prev_last_four_first)[0] + "\n" + second_tool + "\n" + prev_last_four_second + "\n" + n_layer.split(first_tool)[0]
                 active_tool = first_tool
 
             elif active_tool == first_tool and second_tool in layer and init_state == 1:
                 #Create a str from layer after the second tool line number to the last four "\n" lines before ";TIME_ELAPSED:"
                 new_layer = first_tool + "\n" + prev_last_four_first + layer
-                prev_last_four_second = '\n'.join(lines[-5:])
+                for line_number, line in enumerate(lines):
+                    if "Z" in line:
+                        last_z_line = line_number
+                prev_last_four_second = '\n'.join(lines[last_z_line:])
                 new_layer = new_layer.split(prev_last_four_second)[0]
                 active_tool = second_tool
 
